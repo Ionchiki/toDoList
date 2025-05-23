a914b8078cd4129b089fd9ef6fde74a46726efb5
George Ionov <georgijionov603@gmail.com>
2025-05-23 19:00:40 +0300

    add proj

--- back/server.js
+++ back/server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`);
 });
